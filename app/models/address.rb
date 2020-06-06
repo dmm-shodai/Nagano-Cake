f304ce8285341790e39c5c4872a2a2e5dc2b4592
natsumi minowa <apple@appurunoMacBook-Air.local>
2020-06-06 21:09:05 +0900

    add view bootstrap

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,6 @@
 class Address < ApplicationRecord
-	belongs_to :member
+	belongs_to :member,optional: true
+	# optional: trueはあとで抜く
 
 	validates :postal_code, presence: true,length: {is: 7 }
 	validates :address, presence: true
