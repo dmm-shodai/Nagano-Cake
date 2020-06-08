f642133d779b95f1fdce8833e6a8e500ba08b7ae
KodamaHiroaki <kdm@KodamaHiroakinoMacBook-Air.local>
2020-06-09 00:58:30 +0900

    making_member_edit,show,top

--- app/controllers/member/members_controller.rb
+++ app/controllers/member/members_controller.rb
@@ -1,22 +1,29 @@
 class Member::MembersController < ApplicationController
 	def top
+		@items = Items.all
 	end
 
 	def show
 	end
 
 	def edit
-		
+		@member = current_member
+	end
+
+	def update
+		@member = current_member
+		@member.update
+		redirect_to 
 	end
 
 	def hide
-		@members = current_members
-		@members.destroy
+		@member = current_member
+		@member.destroy
 		redirect_to root_path
 	end
 
 	private
 	def member_params
-		params.require(:member).permit(:surname, :name, :kana_surname, :kana_name, :postal_code, :address, :phone, :quit)
+		params.require(:member).permit(:surname, :name, :kana_surname, :kana_name, :email, :postal_code, :address, :phone)
 	end
 end
