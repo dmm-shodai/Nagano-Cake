5c64f1d8e045f481f15172960b2739b89f7c05d5
Yasuyamakyosuke <toyojindai@gmail.com>
2020-06-09 21:16:52 +0900

    owaowari

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,2 +1,18 @@
 class Admin::AdminController < ApplicationController
+
+	def top
+	end
+
+	def index
+		@members = Member.all
+	end
+
+	def show
+	end   
+
+	def edit 
+	end
+
+	def update
+	end
 end
